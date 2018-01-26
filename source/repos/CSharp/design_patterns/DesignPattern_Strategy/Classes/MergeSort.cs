43debc852d306e9d831c106c301875117dc50aa4
Ruy Almeida <ruy.fsalmeida@gmail.com>
2018-01-26 11:17:44 -0200

    MergeSort addition

---
+++ source/repos/CSharp/design_patterns/DesignPattern_Strategy/Classes/MergeSort.cs
@@ -0,0 +1,57 @@
+﻿namespace DesignPattern_Strategy.Classes
+{
+    using DesignPattern_Strategy.Interfaces;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MergeSort : ISorter<int>
+    {
+        public IList<int> Sort(IList<int> input)
+        {
+            if (input.Count == 1)
+                return input;
+
+            int[] firstPart = input.Take(input.Count / 2).ToArray();
+            int[] secondPart = input.Skip(input.Count / 2).ToArray();
+
+            firstPart = Sort(firstPart.ToList()).ToArray();
+            secondPart = Sort(secondPart.ToList()).ToArray();
+
+            return MergeArrays(firstPart, secondPart);
+        }
+
+        private IList<int> MergeArrays(int[] firstPart, int[] secondPart)
+        {
+            List<int> firstPartList = firstPart.ToList();
+            List<int> secondPartList = secondPart.ToList();
+            List<int> mergedList = new List<int>();
+
+            while (firstPartList.Count > 0 && secondPartList.Count > 0)
+            {
+                if (firstPartList[0] > secondPartList[0])
+                {
+                    mergedList.Add(secondPartList[0]);
+                    secondPartList.RemoveAt(0);
+                    continue;
+                }
+
+                mergedList.Add(firstPartList[0]);
+                firstPartList.RemoveAt(0);
+            }
+
+            while (firstPartList.Count > 0)
+            {
+                mergedList.Add(firstPartList[0]);
+                firstPartList.RemoveAt(0);
+            }
+
+            while (secondPartList.Count > 0)
+            {
+                mergedList.Add(secondPartList[0]);
+                secondPartList.RemoveAt(0);
+            }
+
+            return mergedList;
+        }
+    }
+}
