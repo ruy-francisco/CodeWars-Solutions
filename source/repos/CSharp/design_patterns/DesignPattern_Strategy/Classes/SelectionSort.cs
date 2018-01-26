bb1439f654a3c4a9e7b6ff7fce7832d5ac5c124b
Ruy Almeida <ruy.fsalmeida@gmail.com>
2018-01-26 09:42:53 -0200

    Selection Sort implementation

---
+++ source/repos/CSharp/design_patterns/DesignPattern_Strategy/Classes/SelectionSort.cs
@@ -0,0 +1,36 @@
+﻿namespace DesignPattern_Strategy.Classes
+{
+    using DesignPattern_Strategy.Interfaces;
+    using System.Collections.Generic;
+
+    public class SelectionSort : ISorter<int>
+    {
+        public IList<int> Sort(IList<int> input)
+        {
+            int lowestNumber = 0;
+            int lowestIndex = 0;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                lowestNumber = input[i];
+
+                for (int j = i + 1; j < input.Count; j++)
+                {
+                    if (input[j] < lowestNumber)
+                    {
+                        lowestNumber = input[j];
+                        lowestIndex = input.IndexOf(lowestNumber);
+                    }
+                }
+
+                if (lowestNumber != input[i])
+                {
+                    input[lowestIndex] = input[i];
+                    input[i] = lowestNumber;
+                }
+            }
+
+            return input;
+        }
+    }
+}
--- source/repos/CSharp/design_patterns/DesignPattern_Strategy/Program.cs
+++ source/repos/CSharp/design_patterns/DesignPattern_Strategy/Program.cs
@@ -3,8 +3,8 @@
     using DesignPattern_Strategy.Classes;
     using DesignPattern_Strategy.Interfaces;
     using System;
-    using System.Collections.Generic;
     using System.Linq;
+    using System.Diagnostics;
 
     class Program
     {
@@ -12,19 +12,40 @@
         {
             int[] input = { 4, 54, 1, 22 , 12, 11, 15, 2, 53 };
 
-            UserSort(input, new InsertionSort());      
+            var watcher = Stopwatch.StartNew();
+            UserSort(input, new InsertionSort());
+            watcher.Stop();
+            var insertionSortElapsedMilliseconds = watcher.ElapsedMilliseconds;
+
+            Console.WriteLine();
+
+            watcher.Restart();
+            UserSort(input, new BubbleSort());
+            watcher.Stop();
+            var bubbleSortElapsedMilliseconds = watcher.ElapsedMilliseconds;
+
+            Console.WriteLine();
+
+            watcher.Restart();
+            UserSort(input, new SelectionSort());
+            watcher.Stop();
+            var selectionSortElapsedMilliseconds = watcher.ElapsedMilliseconds;
+
+            Console.WriteLine();
+            Console.WriteLine("Insertion sort elapsed time: {0}", insertionSortElapsedMilliseconds);
+            Console.WriteLine("bubble sort elapsed time: {0}", bubbleSortElapsedMilliseconds);
+            Console.WriteLine("Selection sort elapsed time: {0}", selectionSortElapsedMilliseconds);
 
             Console.ReadKey();
         }
 
         private static void UserSort(int[] input, ISorter<int> sorter)
         {
-            input = sorter.Sort(input.ToList()).ToArray();
+            int[] inputToBeSorted = input;
+            int[] sortedArray = sorter.Sort(inputToBeSorted.ToList()).ToArray();
 
             for (int i = 0; i < input.Count(); i++)
-            {
-                Console.Write(input[i] + " ");
-            }
+                Console.Write(sortedArray[i] + " ");
         }
     }
 }
