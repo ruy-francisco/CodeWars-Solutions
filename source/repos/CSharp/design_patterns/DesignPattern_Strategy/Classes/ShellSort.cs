67d1d3467ca87ffbb7c693e01112b3aed8ebafcb
Ruy Almeida <ruy.fsalmeida@gmail.com>
2018-02-16 14:33:46 -0200

    ShellSort implementation

---
+++ source/repos/CSharp/design_patterns/DesignPattern_Strategy/Classes/ShellSort.cs
@@ -0,0 +1,42 @@
+﻿namespace DesignPattern_Strategy.Classes
+{
+    using DesignPattern_Strategy.Interfaces;
+    using System.Collections.Generic;
+
+    public class ShellSort : ISorter<int>
+    {
+        public IList<int> Sort(IList<int> input)
+        {
+            int interval = 1;
+
+            while (interval < (input.Count / 3))
+            {
+                interval = interval * 3 + 1;
+            }
+
+            int valueToInsert = 0;
+            int inner = 0;
+
+            while (interval > 0)
+            {
+                for (int outer = interval; outer < input.Count; outer++)
+                {
+                    valueToInsert = input[outer];
+                    inner = outer;
+
+                    while ((inner > interval - 1) && (input[inner - interval] >= valueToInsert))
+                    {
+                        input[inner] = input[inner - interval];
+                        inner -= interval;
+                    }
+
+                    input[inner] = valueToInsert;
+                }
+
+                interval = (interval - 1) / 3;
+            }
+
+            return input;
+        }
+    }
+}
