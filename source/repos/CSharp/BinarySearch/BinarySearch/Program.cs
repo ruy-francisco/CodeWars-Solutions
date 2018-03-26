de8cffc557492b34f440c120c6bb8398351412e6
Ruy Almeida <ruy.fsalmeida@gmail.com>
2018-03-26 10:36:58 -0300

    Using binary search for strings

--- source/repos/CSharp/BinarySearch/BinarySearch/Program.cs
+++ source/repos/CSharp/BinarySearch/BinarySearch/Program.cs
@@ -12,6 +12,7 @@ namespace BinarySearch
             try
             {
                 List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 56, 78, 79, 100 };
+                List<string> names = new List<string> { "Carlos", "Fernando", "Fúlvia", "Fred", "Ruy", "Zelda" };
 
                 int numberToSearch = 0;
 
@@ -21,6 +22,10 @@ namespace BinarySearch
                 int.TryParse(typedNumbers, out numberToSearch);
 
                 Console.WriteLine("O índice do número procurado é: {0}", numbers.GetIndexForValue(numberToSearch));
+
+                Console.Write("Digite um nome para ser procurado: ");
+                string typedName = Console.ReadLine();
+                Console.WriteLine("O índice do número procurado é: {0}", names.GetIndexForValue(typedName));
 
                 Console.ReadKey();
             }
